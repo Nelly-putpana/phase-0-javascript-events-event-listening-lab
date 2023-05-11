6641650f29b788544c20611d23fb50983669167e
Nelly Putpana <nelly.putpana@gmail.com>
2023-05-10 20:39:09 -0600

    javas eventlistener

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+// function addingEventListener() {
+// alert('I was clicked!');}
+
+const input = document.getElementById('button');
+
 function addingEventListener() {
-}
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    input.addEventListener('click', clickAlert);
+};
+
+addingEventListener();
+ 
